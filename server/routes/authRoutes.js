a9a293a8d67652b8a6907089170627064124f50c
francisco.barbosa <francisco.barbosa@kuehne-nagel.com>
2024-08-18 12:46:49 +0100

    Add User registration endpoint

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const basicAuth = require('../middleware/auth');
+const {registerUser} = require('../controllers/authController');
+
+router.post('/user', registerUser);
 
 router.get('/protected', basicAuth, (req, res) => {
     res.send('This is a protected route');
